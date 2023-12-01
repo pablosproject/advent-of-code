6779be9af7dcb6ae9befffb6acff653911108203
Paolo Tagliani <pablosproject@gmail.com>
2023-12-01 17:00:21 +0100

    Day 01 part 2

--- 01/01.go
+++ 01/01.go
@@ -5,8 +5,26 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
+
+type numbersWord struct {
+	word  string
+	value string
+}
+
+var numbers = []numbersWord{
+	{"one", "1"},
+	{"two", "2"},
+	{"three", "3"},
+	{"four", "4"},
+	{"five", "5"},
+	{"six", "6"},
+	{"seven", "7"},
+	{"eight", "8"},
+	{"nine", "9"},
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -27,19 +45,59 @@ func main() {
 	fmt.Printf("%d\n", number)
 }
 
-func processNumber(number string) int {
-	re := regexp.MustCompile("[0-9]")
-	numbers := re.FindAllString(number, -1)
+type numberPos struct {
+	position int
+	number   string
+}
+
+func processNumber(line string) int {
+	numbers := searchNumbersOccurrence(line)
+	numbersWord := searchNumberWordOccurrence(line)
+
+	numbers = append(numbers, numbersWord...)
+	sort.Slice(numbers, func(i, j int) bool {
+		return numbers[i].position < numbers[j].position
+	})
 
 	last := numbers[len(numbers)-1]
 	first := numbers[0]
 
-	num := first + last
+	num := first.number + last.number
 
 	res, err := strconv.Atoi(num)
 	if err != nil {
 		panic(err)
 	}
 
+	return res
+}
+
+func searchNumberWordOccurrence(line string) []numberPos {
+	res := []numberPos{}
+
+	for _, numWord := range numbers {
+		re := regexp.MustCompile(numWord.word)
+
+		positions := re.FindAllStringIndex(line, -1)
+
+		for _, pos := range positions {
+			position := pos[0]
+			value := numWord.value
+			res = append(res, numberPos{position, value})
+		}
+	}
+	return res
+}
+
+func searchNumbersOccurrence(line string) []numberPos {
+	re := regexp.MustCompile("[0-9]")
+	numbers := re.FindAllStringIndex(line, -1)
+
+	res := []numberPos{}
+	for _, numberLoc := range numbers {
+		position := numberLoc[0]
+		char := line[position]
+		res = append(res, numberPos{position, string(char)})
+	}
 	return res
 }
--- 01/01_test.go
+++ 01/01_test.go
@@ -7,6 +7,13 @@ func TestProcessNumber(t *testing.T) {
 		input  string
 		output int
 	}{
+		{"two1nine", 29},
+		{"eightwothree", 83},
+		{"abcone2threexyz", 13},
+		{"xtwone3four", 24},
+		{"4nineeightseven2", 42},
+		{"zoneight234", 14},
+		{"7pqrstsixteen", 76},
 		{"1abc2", 12},
 		{"pqr3stu8vwx", 38},
 		{"a1b2c3d4e5f", 15},
